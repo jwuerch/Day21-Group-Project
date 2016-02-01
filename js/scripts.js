185dc0efa7fe5838b86381825ddb932e95c8efdb
erik <tolentino3575@gmail>
2016-02-01 14:24:21 -0800

    Finish jQuery, site works

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-function Students(coreLanguage, languageSelect) {
+function Students(coreLanguage) {
   this.coreLanguage = coreLanguage;
-  this.languageSelect = languageSelect;
 }
 
-
-
-
-if (this.languageSelect === "java") {
-
+Students.prototype.pickLanguage = function (input) {
+  this.coreLanguage = input;
+  return this.coreLanguage;
 }
-
-
 
 $(document).ready(function() {
   $("form#jobSearch").submit(function(event) {
+    $(".ruby").hide();
+    $(".cSharp").hide();
+    $(".php").hide();
+    $(".java").hide();
+    $(".javascript").hide();
+    $(".css").hide();
 
+    var input = $("#language").val()
+    if(input === "java") {
+      $(".java").show();
+    } else if (input === "javascript") {
+      $(".javascript").show();
+    } else if (input === "ruby") {
+      $(".ruby").show();
+    } else if (input === "php") {
+      $(".php").show();
+    } else if (input === "cSharp") {
+      $(".cSharp").show();
+    } else if (input === "css")
+      $(".css").show();
+        event.preventDefault();
+    });
   });
-});
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,12 @@
 describe ('Students', function(){
+
   it("will create students object based on specific parameters", function(){
     var testStudents = new Students("java");
     expect(testStudents.coreLanguage).to.equal("java");
   });
 
   it("allow user to select their core language and will display links with job openings", function(){
-    var testStudents = new Students("java");
-    expect(testStudents.languageSelect).to.equal("java");
+    var testStudents = new Students("ruby");
+    expect(testStudents.pickLanguage("ruby")).to.equal("ruby");
   });
 });
