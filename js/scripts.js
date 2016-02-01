1f690fa7f16e024a62896b950787b217bb6e7fff
jason & alex <jasonandalex@gmail.com>
2016-02-01 14:19:28 -0800

    add jquery effects for job postings and resources columns

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
-var drawPaginationLinks = function (result) {
-  var totalResults = result.totalResults,
-  perPage = 10,
-  numPages = Math.ceil(totalResults / perPage),
-  $links = $('<ul></ul>');
-  for ( var i = 1; i <= numPages; i++ ) {
-    $links.append('<li><a href="#job-search">' + i + '</a></li>');
-  }
-  $links.children('li').eq(0).children('a').addClass('active');
-  $resultLinks.append($links);
-};
-
-var doSearch = function (params, done, fail) {
-  $.ajax({ [Initial Parameters] }, params),
-    dataType: 'jsonp',
-    type: 'GET',
-    timeout: 5000,
-    url: 'http://api.indeed.com/ads/apisearch'
-  }).done(done).fail(fail);
-
+// var drawPaginationLinks = function (result) {
+//   var totalResults = result.totalResults,
+//   perPage = 10,
+//   numPages = Math.ceil(totalResults / perPage),
+//   $links = $('<ul></ul>');
+//   for ( var i = 1; i <= numPages; i++ ) {
+//     $links.append('<li><a href="#job-search">' + i + '</a></li>');
+//   }
+//   $links.children('li').eq(0).children('a').addClass('active');
+//   $resultLinks.append($links);
+// };
+//
+// var doSearch = function (params, done, fail) {
+//   $.ajax({ [Initial Parameters] }, params),
+//     dataType: 'jsonp',
+//     type: 'GET',
+//     timeout: 5000,
+//     url: 'http://api.indeed.com/ads/apisearch'
+//   }).done(done).fail(fail);
+//
 
 
 $(function () {
-
-$("button").click(function () {
-  var param = $("#search").val()
-  $("button").append(drawPaginationLinks())
-}
-
-
+  $("button").click(function () {
+    $("#row-2").fadeOut(400);
+    $("#row-3").fadeIn(1600);
+    $("#row-3").addClass("animated slideInUp");
+  });
 });
