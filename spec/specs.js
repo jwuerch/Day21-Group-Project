96c45f0a8fe485719360a0818e495877ba46f9e0
erik <tolentino3575@gmail>
2016-02-01 16:43:37 -0800

    Add specs and pass

--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
-describe("apiCall", function () {
-it("will take in search querie and call indeed.com with that value", function () {
-  expect(apiCall.status).to.equal(200);
-});
-=======
-describe ('Students', function(){
+// describe("apiCall", function () {
+// it("will take in search querie and call indeed.com with that value", function () {
+//   expect(apiCall.status).to.equal(200);
+//   });
+// });
+
+describe ("Students", function(){
 
   it("will create students object based on specific parameters", function(){
     var testStudents = new Students("java");
@@ -15,5 +15,17 @@ describe ('Students', function(){
     var testStudents = new Students("ruby");
     expect(testStudents.pickLanguage("ruby")).to.equal("ruby");
   });
->>>>>>> 185dc0efa7fe5838b86381825ddb932e95c8efdb
+});
+
+describe("contactForm", function (){
+  it("will create contactForm object given specific properties", function () {
+    var testcontactForm = new contactForm ("John Taylor", "Test City", "Test Field Interest");
+    expect(testcontactForm.fullName).to.equal("John Taylor");
+    expect(testcontactForm.city).to.equal("Test City");
+    expect(testcontactForm.field).to.equal("Test Field Interest");
+  });
+    it("adds the fullcontactForm method to all specifications above", function() {
+    var testcontactForm = new contactForm("John Taylor", "Test City","Test Field Interest");
+    expect(testcontactForm.fullcontactForm()).to.equal("John Taylor, Test City, Test Field Interest");
+  });
 });
